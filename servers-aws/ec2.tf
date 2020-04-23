f7af84cd1abb858e4470b3325824860149526ade
caiosbentes <caiosbentes@gmail.com>
2020-04-23 09:55:18 -0700

    adicição de módulo data

--- servers-aws/ec2.tf
+++ servers-aws/ec2.tf
@@ -1,15 +1,22 @@
 resource "aws_instance" "server" {
-  count                  = var.servers
-  ami                    = var.ami
-  instance_type          = var.instance_type
-  key_name               = var.ami_key_pair_name
-  vpc_security_group_ids = [aws_security_group.desafio-sg.id]
+  count                       = var.servers
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.ami_key_pair_name
+  vpc_security_group_ids      = [aws_security_group.desafio-sg.id]
   associate_public_ip_address = true
-  user_data = file("script.sh")
+  subnet_id                   = aws_subnet.desafio-subnet.id
+  user_data                   = data.template_file.script.rendered
+
   tags = {
     Name        = var.name
     Environment = var.env
     Provisioner = "Terraform"
   }
-  subnet_id = aws_subnet.desafio-subnet.id
 }
+
+data "template_file" "script" {
+  template = file("script.sh")
+}
+
+

7f95dc201a40ad1cf93c1c89e4aa033ebf964b8a
Caio Sanches Bentes <caiosbentes@users.noreply.github.com>
2020-04-24 15:23:51 -0300

    kemove key_name output

--- servers-aws/output.tf
+++ servers-aws/output.tf
@@ -2,7 +2,3 @@ output "ip_address" {
   description = "IP da instancia criada"
   value = "${aws_instance.server[*].public_ip}"
 }
-output "key_name" {
-  description = "key name da instancia"
-  value       = "${aws_instance.server[*].key_name}"
-}
